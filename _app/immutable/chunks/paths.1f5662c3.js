80a19a7c3407dc5fed7b76d4be4a2f38e0c63ae1
ENPANADA <92269511+ENPANADA@users.noreply.github.com>
2023-10-14 00:57:31 -0600

    Add files via upload

---
+++ _app/immutable/chunks/paths.1f5662c3.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1grz30e)==null?void 0:s.base)??"/The_Simpsons";var e;const t=((e=globalThis.__sveltekit_1grz30e)==null?void 0:e.assets)??a;export{t as a,a as b};
